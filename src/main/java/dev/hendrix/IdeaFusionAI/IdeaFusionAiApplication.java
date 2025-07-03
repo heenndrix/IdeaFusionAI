e3c1a079feb8d4aa754317f36e638d8bcd0ffa25
heenndrix <hendrixoliveira89@gmail.com>
2025-07-03 16:20:31 -0300

    fix: erro no arquivo main concertado

--- src/main/java/dev/hendrix/IdeaFusionAI/IdeaFusionAiApplication.java
+++ src/main/java/dev/hendrix/IdeaFusionAI/IdeaFusionAiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IdeaFusionAiApplication {
 
 
-	//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
 	public static void main(String[] args) {
 		SpringApplication.run(IdeaFusionAiApplication.class, args);
 	}
